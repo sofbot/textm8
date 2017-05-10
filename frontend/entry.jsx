5dfe35c5cbf90c9f70fdf89064d8430d8ff6b99d
Sofia Chen <me@sofiachen.com>
2017-05-10 15:44:24 -0700

    add tested API Utils

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as APIUtil from './util/edia_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
+  window.getAllTexts = APIUtil.getAllTexts;
+  window.createText = APIUtil.createText;
+  window.updateText = APIUtil.updateText;
+  window.getOneText = APIUtil.getOneText;
+  window.deleteText = APIUtil.deleteText;
+  window.searchTexts = APIUtil.searchTexts;
+
   const root = document.getElementById('root');
   ReactDOM.render(<h1>yas knowble</h1>, root);
 });
---
+++ frontend/util/edia_api_util.js
@@ -0,0 +1,37 @@
+export const getAllTexts = () => (
+  fetch('http://localhost:8080/text')
+  .then(response => response.json())
+);
+
+export const createText = text => (
+  fetch('http://localhost:8080/text', {
+    method: 'POST',
+    body: JSON.stringify(text),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+);
+
+// text object must include text.id
+export const updateText = text => (
+  fetch('http://localhost:8080/text', {
+    method: 'PUT',
+    body: JSON.stringify(text),
+    headers: new Headers({
+      'Content-Type': 'application/json'
+    })
+  })
+);
+
+export const getOneText = id => (
+  fetch(`http://localhost:8080/text/${id}`)
+  .then(response => response.json())
+  .then(text => text)
+);
+
+export const deleteText = id => (
+  fetch(`http://localhost:8080/text/${id}`, {
+    method: 'DELETE'
+  })
+);
